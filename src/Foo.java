e790a4ef1257dcbc16acd535e61e8fbef78739cf
Selma Čopra <selmacopra27@gmail.com>
2023-04-24 21:33:22 +0200

    solution v1

--- src/Foo.java
+++ src/Foo.java
@@ -1,8 +1,31 @@
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Foo {
 
     public static int isBuyerWinner(List<String> codeList, List<String> shoppingCart) {
-        return 0;
+        if (codeList.isEmpty())
+            return 1;
+        else if (shoppingCart.isEmpty())
+            return 0;
+
+        String shoppingString = String.join(" ", shoppingCart);
+        Pattern pattern;
+        Matcher matcher;
+
+        for (String codeString : codeList) {
+            String patternString = codeString.replaceAll("anything", "\\\\w+");
+            pattern = Pattern.compile(patternString);
+            matcher = pattern.matcher(shoppingString);
+
+            if (matcher.find()) {
+                shoppingString = shoppingString.substring(matcher.end()).trim();
+            } else {
+                return 0;
+            }
+        }
+
+        return 1;
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -7,18 +7,22 @@ public class Solution {
 
         Scanner keyboard = new Scanner(System.in);
 
+        System.out.println("Enter number of code lists: ");
         int codeListCount = Integer.parseInt(keyboard.nextLine().trim());
         List<String> codeList = new ArrayList<>();
 
         for (int i = 0; i < codeListCount; i++) {
+            System.out.println("Enter code list: ");
             String codeListItem = keyboard.nextLine();
             codeList.add(codeListItem);
         }
 
+        System.out.println("Enter how many shopping items you will have: ");
         int shoppingCardCount = Integer.parseInt(keyboard.nextLine().trim());
         List<String> shoppingCart = new ArrayList<>();
 
         for (int i = 0; i < shoppingCardCount; i++) {
+            System.out.println("Enter shopping item: ");
             String shoppingCartItem = keyboard.nextLine();
             shoppingCart.add(shoppingCartItem);
         }
@@ -26,6 +30,5 @@ public class Solution {
         int result = Foo.isBuyerWinner(codeList, shoppingCart);
 
         System.out.println(result);
-        System.out.println(codeList.toString());
     }
 }
